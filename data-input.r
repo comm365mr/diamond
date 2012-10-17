37b5a112cd9f467671de50e7a400b4df867306d0
Chunhua Wu <chunhua0319@gmail.com>
2012-10-17 09:50:53 -0700

    commit 2

--- data-input.r
+++ data-input.r
@@ -1,4 +1,23 @@
 # Let us import data into R
 # Method 1, you can use workspace ->import dataset 
 # Method 2, type commands
+diamond <- read.csv("diamond.csv")
 diamond_price <- read.csv("diamond-price.csv")
+
+# simple data check
+head(diamond)
+
+# data transformation
+logprice = log(diamond$price)
+diamond$logprice = logprice
+
+
+# data summary
+summary(diamond$color)
+
+# histogram
+hist(diamond$carat, main="Histogram for Diamond Carat", xlab="Carat", col="lightblue")
+
+## section 101 stopped here on Oct 17th. 
+
+
