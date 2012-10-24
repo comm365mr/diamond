4779a0c51156dcc1da74c622e458611dd8c1e707
Chunhua Wu <chunhua@chunhuawu.com>
2012-10-24 01:18:28 -0700

    added codes for hypothesis testing

--- testing.r
+++ testing.r
@@ -1,31 +1,59 @@
-### Codes used in the hypothesis testing lecture
+### Codes used in the hypothesis testing lecture (lecture 12)
+## please run codes in data_input.r first, you can choose "source" option to run all of them
+## this is important, as we are going to use data created in data_input.r file
 
-### Merge data to create a file to track the duration a diamond is sold
-## create a data called sales, which contains the following variables
-## 1. sku
-## 2. number of days observed
-## 3. whether the diamond is sold at the end of the day: 0, 1 variable
+# 1. t-test for single mean
+
+t.test(sales$days, mu=10, alternative="greater")
+# sales$days is the data to use
+# mu=10: you specify the hypothesized mu
+# alternative="greater": specify the direction for alternative
+t.test(sales$days, mu=19, alternative="two.sided")
 
 
-## change the date variable from string to dates
-diamond$fetch_date = as.Date(diamond$fetch_date, "%d/%m/%Y")
-price$fetch_date = as.Date(price$fetch_date, "%d/%m/%Y")
+# 2. t-test for difference between means
+## create a variable called "month" on diamond data using months() function
+diamond$month = months(diamond$fetch_date)
+## testing for independent samples
+t.test(diamond$price[diamond$month=="July"], diamond$price[diamond$month=="August"])
+## diamond$price[diamond$month=="July"] means get the subset of diamond$price based on the condition 
+## that diamond$month equals "July". 
 
-summary(diamond$fetch_date)
-summary(price$fetch_date)
-days = aggregate(fetch_date~sku, data=price, length)
-names(days) = c("sku", "days")
-last_day = aggregate(fetch_date~sku, data=price, max)
-names(last_day) = c("sku", "last_day")
-last_day$sold = as.numeric(last_day$last_day< as.Date("2012-09-04"))
-summary(last_day)
+## testing for paired samples
+## create price.day1 and price.day11 data
+## first merge the sales data back to price data
+price = merge(price, sales)
+## extract price information for the first day observations
+price.day1 = price[price$fetch_date==price$first_day, c("sku", "price")]
+## extract price information for the 11th day
+price.day11 = price[price$fetch_date==as.Date(price$first_day+10), c("sku", "price")]
+## get common observations for both day 1 and day 11
+price.day1 = price.day1[price.day1$sku %in% price.day11$sku, ]
+dim(price.day1)
+## sort to make sure they are in the same order
+price.day1 = price.day1[order(price.day1$sku),]
+price.day11 = price.day11[order(price.day11$sku), ]
+## Results in 2692 diamonds
 
-days = merge(days, last_day)
-summary(days)
-summary(days$days[days$sold==1])
+t.test(price.day1$price, price.day11$price, paired=T, alternative="greater")
 
-t.test(days$days, mu=20, alternative='greater')
 
-mean(days$days)
+###testing for proportion
+with(sales[sales$first_day<as.Date("2012-08-25"), ],
+     prop.test(sum(days<=10), length(days), p=0.3))
+# use with() function to select the subsample
+# sum(days<=0) is the total number of observations sold within 10 days 
+
+
+
+### Chisq test: presidential votes
+# create the votes data
+votes = matrix(c(110, 230, 160, 185, 150, 165, 265, 115, 120), 3,3)
+# chi-square test
+chisq.test(votes)
+
+
+
+
 
 
